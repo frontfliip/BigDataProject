ca20db9ad9a582ca431629ecae4e92afe2fa1ab6
liubomyr.mokrytskyi <liubomyr.mokrytskyi@ucu.edu.ua>
2024-05-23 22:14:37 +0300

    - reformatted projects riles - creating MVC principle

--- DataExtraction/run.sh
+++ DataExtraction/run.sh
@@ -1,5 +1,5 @@
 echo "Build extraction container..."
-docker build -t multi_websocket_to_kafka .
+docker build -f RestAPI/Dockerfile -t multi_websocket_to_kafka .
 docker run --network project-net --name multi_websocket_to_kafka_container multi_websocket_to_kafka
 echo "Extraction is started!"
 sleep 10
