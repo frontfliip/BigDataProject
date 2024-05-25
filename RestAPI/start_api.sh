6babd49656e64cbc5d2457c80daa6c2e1bdd6586
liubomyr.mokrytskyi <liubomyr.mokrytskyi@ucu.edu.ua>
2024-05-25 11:17:06 +0300

    - formatting of forgotten files

--- RestAPI/start_api.sh
+++ RestAPI/start_api.sh
@@ -3,8 +3,6 @@ docker build -f RestAPI/Dockerfile.api . -t restapi
 echo "cool"
 echo "Creating network for Rest API"
 sleep 5
-#docker run --name rest_api --network project-net -p 1488:1488 restapi
-#docker run --name rest_api --network project-net restapi
-#sleep 20
 #use this one to see logs
 #docker run --name rest_api --network project-net -p 1488:1488 --log-driver=local --log-opt max-size=10m restapi
+#sleep 5
